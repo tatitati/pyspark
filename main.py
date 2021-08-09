5a10fa7bd2c51aa3789e41b7f85412b53f8a9283
Francisco Albert Albusac <francis.jaa@protonmail.com>
2021-08-09 23:06:24 +0100

    load data into a df from a csv

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from pyspark.sql import SparkSession
 # start an spark session
 spark = SparkSession.builder.appName("firstApp").getOrCreate()
 
+# load data into a datafrom from a csv
+# financial data here: https://finance.yahoo.com/quote/GOOG?p=GOOG&.tsrc=fin-srch
+df = spark.read.csv("GOOG.csv", header=True, inferSchema=True)
+print(df)
 
 
 
