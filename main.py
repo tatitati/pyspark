4a22a94f12bec12ace75d59f520513d2dc823c57
Francisco Albert Albusac <francis.jaa@protonmail.com>
2021-08-09 23:07:41 +0100

    columns

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ spark = SparkSession.builder.appName("firstApp").getOrCreate()
 # load data into a datafrom from a csv
 # financial data here: https://finance.yahoo.com/quote/GOOG?p=GOOG&.tsrc=fin-srch
 df = spark.read.csv("GOOG.csv", header=True, inferSchema=True)
-print(df)
+print(df) # DataFrame[Date: string, Open: double, High: double, Low: double, Close: double, Adj Close: double, Volume: int]
 
 # perform basic dataframe operations
-
+print(df.columns) # ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
 
 
