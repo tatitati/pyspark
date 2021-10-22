c8d67fdd1613ee7fee79104f841feb06edccccff
Francisco Albusac <francisco.albusac@bbc.co.uk>
2021-10-22 15:50:03 +0200

    numpy stuff

--- course2/main.py
+++ course2/main.py
@@ -78,6 +78,39 @@ mydata2.show()
 # |  5|  Sherwood|   Macieja|  Male|      Mytishchi|            VP Sales|$63863.09|      null| 37.6489954|      Mytishchi|    63863.09|  63863.08984375|
 # |  6|     Maris|      Folk|Female|Kinsealy-Drinan|      Civil Engineer|$30101.16|53.4266145| -6.1644997|Kinsealy-Drinan|    30101.16|  30101.16015625|
 
+import numpy as np
+latitudes = mydata2.select('Latitude')
+latitudes.show()
+# +----------+
+# |  Latitude|
+# +----------+
+# |50.5774075|
+# |48.8231572|
+# |44.5047212|
+# |      null|
+# |53.4266145|
+
+latitudes = latitudes.filter(latitudes.Latitude.isNotNull())
+latitudes.show()
+# +----------+
+# |  Latitude|
+# +----------+
+# |50.5774075|
+# |48.8231572|
+# |44.5047212|
+# |53.4266145|
 
 
+latitudes = latitudes.withColumn('latitude2', latitudes.Latitude.cast('float')).select('latitude2')
+median = np.median(latitudes.collect())
+print(median)
+# 31.93397331237793
 
+mydata2 = mydata2.withColumn('lat', when(mydata2.Latitude.isNull(), lit(median)).otherwise(mydata2.Latitude))
+mydata2.show()
+# +---+----------+----------+------+---------------+--------------------+---------+----------+-----------+---------------+------------+----------------+-----------------+
+# | id|first_name| last_name|gender|           City|            JobTitle|   Salary|  Latitude|  Longitude|     clean_city|clean_salary|      new_salary|              lat|
+# +---+----------+----------+------+---------------+--------------------+---------+----------+-----------+---------------+------------+----------------+-----------------+
+# |  1|   Melinde| Shilburne|Female|      Nowa Ruda| Assistant Professor|$57438.18|50.5774075| 16.4967184|      Nowa Ruda|    57438.18|   57438.1796875|       50.5774075|
+# |  2|  Kimberly|Von Welden|Female|         Bulgan|       Programmer II|$62846.60|48.8231572|103.5218199|         Bulgan|     62846.6|   62846.6015625|       48.8231572|
+# |  4|   Shannon| O'Griffin|  Male|  Divnomorskoye|Budget/Accounting...|$61489.23|44.5047212| 38.1300171|  Divnomorskoye|    61489.23|  61489.23046875|       44.5047212|
