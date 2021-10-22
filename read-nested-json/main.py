57c63951cab5282ba97723174613b5cb6195dc35
Francisco Albusac <francisco.albusac@bbc.co.uk>
2021-10-22 19:24:05 +0200

    nested json

---
+++ read-nested-json/main.py
@@ -0,0 +1,41 @@
+#!/usr/local/bin/python3
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.functions import explode
+
+spark = SparkSession.builder.appName("JSONFileRead").master("local").getOrCreate()
+
+df = spark.read.option("multiLine", True).json("dataset.json")
+df.printSchema()
+# root
+#  |-- Education: array (nullable = true)
+#  |    |-- element: struct (containsNull = true)
+#  |    |    |-- Qualification: string (nullable = true)
+#  |    |    |-- year: long (nullable = true)
+#  |-- name: string (nullable = true)
+
+df.show()
+
+flat=df.select(
+    'name',
+    explode('Education').alias('education_flat')
+)
+flat.show()
+# +-------+--------------+
+# |   name|education_flat|
+# +-------+--------------+
+# | Clarke|    {BE, 2011}|
+# | Clarke|    {ME, 2013}|
+# |Michael|    {BE, 2010}|
+# +-------+--------------+
+
+out_df=flat.select('name','education_flat.Qualification', 'education_flat.year')
+out_df.show()
+# +-------+-------------+----+
+# |   name|Qualification|year|
+# +-------+-------------+----+
+# | Clarke|           BE|2011|
+# | Clarke|           ME|2013|
+# |Michael|           BE|2010|
+# +-------+-------------+----+
