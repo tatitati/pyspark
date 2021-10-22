592a65a93c462991939535a5f5364039d94c3368
Francisco Albusac <francisco.albusac@bbc.co.uk>
2021-10-23 00:58:52 +0200

    more

--- read-json-api/main2.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-from pyspark import SparkContext
-from pyspark.sql import functions as F
-from urllib.request import Request, urlopen
-import certifi
-
-sc = SparkContext(master="local[*]", appName= "readJSON")
-spark = SparkSession.builder.getOrCreate()
