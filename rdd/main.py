ae0bac6d46d1b79da579a763d93bad46ebc31db9
Francisco Albusac <francisco.albusac@bbc.co.uk>
2021-10-24 22:52:16 +0200

    more

---
+++ rdd/main.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python3s
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+sparkContext=spark.sparkContext
+rdd=sparkContext.parallelize([1,2,3,4,5])
+rddCollect = rdd.collect()
+
+
+print("Number of Partitions: "+str(rdd.getNumPartitions()))
+# Number of Partitions: 8
+
+print("Action: First element: "+str(rdd.first()))
+# Action: First element: 1
+
+print(rddCollect)
+# [1, 2, 3, 4, 5]
