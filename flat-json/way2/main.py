749b911e320a73c3a387082a13a72e863c936cda
Francisco Albusac <francisco.albusac@bbc.co.uk>
2021-10-26 01:15:51 +0200

    better

--- flat-json/way2/main.py
+++ flat-json/way2/main.py
@@ -3,6 +3,19 @@
 import json
 import pandas as pd
 from json_flatten import flatten
+from pyspark.sql import SparkSession
+import requests
+import json
+from pyspark.sql.functions import udf, col, explode
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, ArrayType
+from pyspark.sql import Row
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import approx_count_distinct,collect_list
+from pyspark.sql.functions import collect_set,sum,avg,max,countDistinct,count
+from pyspark.sql.functions import first, last, kurtosis, min, mean, skewness
+from pyspark.sql.functions import stddev, stddev_samp, stddev_pop, sumDistinct
+from pyspark.sql.functions import variance,var_samp,  var_pop
 
 with open('dataset.json') as f:
     d = json.load(f)
@@ -42,15 +55,23 @@ print(flat_json_dict)
 #   }
 # ]
 
-df = pd.DataFrame(flat_json_dict)
-print(df.head(3))
-#   ProductNum Properties.[0].key  ... Properties.[2].value unitCount
-# 0    6000078         invoice_id  ...               312002         3
-# 1    6000078         invoice_id  ...               312002         3
-# 2    6000078         invoice_id  ...               312002         3
+# df = pd.DataFrame(flat_json_dict)
+spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+rddJson = spark.sparkContext.parallelize([flat_json_dict])
+df = spark.read.json(rddJson)
+
+
+print(df.show())
+# +----------+------------------+--------------------+------------------+--------------------+------------------+--------------------+---------+
+# |ProductNum|Properties.[0].key|Properties.[0].value|Properties.[1].key|Properties.[1].value|Properties.[2].key|Properties.[2].value|unitCount|
+# +----------+------------------+--------------------+------------------+--------------------+------------------+--------------------+---------+
+# |   6000078|        invoice_id|              923659|            job_id|              296160|            sku_id|              312002|        3|
+# |   6000078|        invoice_id|              923659|            job_id|              296160|            sku_id|              312002|        3|
+# |   6000078|        invoice_id|              923659|            job_id|              296160|            sku_id|              312002|        3|
+# +----------+------------------+--------------------+------------------+--------------------+------------------+--------------------+---------+
 
 print(df.columns)
-# Index([
+# [
 #     'ProductNum',
 #     'Properties.[0].key',
 #     'Properties.[0].value',
@@ -58,6 +79,5 @@ print(df.columns)
 #     'Properties.[1].value',
 #     'Properties.[2].key',
 #     'Properties.[2].value',
-#     'unitCount'],
-#     dtype='object'
-# )
+#     'unitCount'
+# ]
